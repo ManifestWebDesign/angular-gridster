23f9617ec38bb8929501cbe59fbaa7db93597796
Dan Blaisdell <dan@manifestwebdesign.com>
2013-11-25 20:34:25 -0800

    more unit tests

--- test/spec/gridster.js
+++ test/spec/gridster.js
@@ -6,7 +6,10 @@ describe('Controller: GridsterCtrl', function() {
 	beforeEach(module('gridster'));
 
 	var GridsterCtrl,
-		scope;
+		scope,
+		item1x1,
+		item2x1,
+		opts;
 
 	// Initialize the controller and a mock scope
 	beforeEach(inject(function($controller, $rootScope) {
@@ -14,7 +17,7 @@ describe('Controller: GridsterCtrl', function() {
 		GridsterCtrl = $controller('GridsterCtrl', {
 			$scope: scope
 		});
-		var opts = {
+		opts = {
 			colWidth: 100,
 			rowHeight: 100,
 			columns: 6,
@@ -25,10 +28,51 @@ describe('Controller: GridsterCtrl', function() {
 			maxRows: 100,
 			mobileBreakPoint: 600
 		};
+		item1x1 = {
+			width: 1,
+			height: 1
+		};
+		item2x1 = {
+			width: 2,
+			height: 1
+		};
 		GridsterCtrl.init(null, null, opts);
+		GridsterCtrl.initializing = false;
 	}));
 
 	it('should have a grid Array', function() {
 		expect(GridsterCtrl.grid.constructor).toBe(Array);
+	});
+
+	it('should be able to place an item', function() {
+		GridsterCtrl.putItem(item1x1, [0, 0]);
+		expect(GridsterCtrl.getItem([0, 0])).toBe(item1x1);
+	});
+
+	it('should be able to place an item without a position', function() {
+		GridsterCtrl.putItem(item1x1);
+		expect(GridsterCtrl.getItem([0, 0])).toBe(item1x1);
+	});
+
+	it('should float an item up', function() {
+		GridsterCtrl.putItem(item1x1, [3, 0]);
+		expect(GridsterCtrl.getItem([0, 0])).toBe(item1x1);
+	});
+
+	it('should stack items when they float up', function() {
+		GridsterCtrl.putItem(item1x1, [3, 0]);
+		expect(GridsterCtrl.getItem([0, 0])).toBe(item1x1);
+
+		GridsterCtrl.putItem(item2x1, [3, 0]);
+		expect(GridsterCtrl.getItem([1, 0])).toBe(item2x1);
+
+		GridsterCtrl.putItem(item1x1, [3, 1]);
+		expect(GridsterCtrl.getItem([1, 1])).toBe(item1x1);
+	});
+
+	it('should not float items if initializing', function() {
+		GridsterCtrl.initializing = true;
+		GridsterCtrl.putItem(item1x1, [3, 0]);
+		expect(GridsterCtrl.getItem([0, 0])).not.toBe(item1x1);
 	});
 });
