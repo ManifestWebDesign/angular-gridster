d36f5148fc829996ccae8bfb16b4a5d5c5e5e429
Dan Blaisdell <dan@manifestwebdesign.com>
2013-12-02 15:35:27 -0800

    updated test controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,63 +2,64 @@
 
 angular.module('app')
 
-	.controller('MainCtrl', function($scope) {
+.controller('MainCtrl', function($scope) {
 
-		$scope.items = [
-			{
-				size: { width: 2, height: 1 },
-				position: [0, 0]
-			},
-			{
-				size: { width: 2, height: 2 },
-				position: [0, 2]
-			},
-			{
-				size: { width: 1, height: 1 },
-				position: [0, 4]
-			},
-			{
-				size: { width: 1, height: 1 },
-				position: [0, 5]
-			},
-			{
-				size: { width: 2, height: 1 },
-				position: [1, 0]
-			},
-			{
-				size: { width: 1, height: 1 },
-				position: [1, 4]
-			},
-			{
-				size: { width: 1, height: 2 },
-				position: [1, 5]
-			},
-			{
-				size: { width: 1, height: 1 },
-				position: [2, 0]
-			},
-			{
-				size: { width: 2, height: 1 },
-				position: [2, 1]
-			},
-			{
-				size: { width: 1, height: 1 },
-				position: [2, 3]
-			},
-			{
-				size: { width: 1, height: 1 },
-				position: [2, 4]
-			}
-		];
+	$scope.items = [
+		{
+			size: { width: 2, height: 1 },
+			position: [0, 0]
+		},
+		{
+			size: { width: 2, height: 2 },
+			position: [0, 2]
+		},
+		{
+			size: { width: 1, height: 1 },
+			position: [0, 4]
+		},
+		{
+			size: { width: 1, height: 1 },
+			position: [0, 5]
+		},
+		{
+			size: { width: 2, height: 1 },
+			position: [1, 0]
+		},
+		{
+			size: { width: 1, height: 1 },
+			position: [1, 4]
+		},
+		{
+			size: { width: 1, height: 2 },
+			position: [1, 5]
+		},
+		{
+			size: { width: 1, height: 1 },
+			position: [2, 0]
+		},
+		{
+			size: { width: 2, height: 1 },
+			position: [2, 1]
+		},
+		{
+			size: { width: 1, height: 1 },
+			position: [2, 3]
+		},
+		{
+			size: { width: 1, height: 1 },
+			position: [2, 4]
+		}
+	];
 
-		$scope.gridsterItemOpts = {
-			width: 'item.size.width',
-			height: 'item.size.height',
-			position: 'item.position'
-		};
+	$scope.gridsterItemOpts = {
+		width: 'item.size.width',
+		height: 'item.size.height',
+		row: 'item.position[0]',
+		column: 'item.position[1]',
+	};
 
-		$scope.gridsterOpts = {
-			margins: [20, 20]
-		};
+	$scope.gridsterOpts = {
+		margins: [20, 20]
+	};
 
-	});
+});
