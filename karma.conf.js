bada83b05afcad1d2ce31121dc38f6c234a6e5b5
Dan Blaisdell <dan@manifestwebdesign.com>
2014-10-02 00:42:52 -0700

    remove jquery-ui dependency and update bower.js "main" property

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,6 @@ module.exports = function(config) {
 		// list of files / patterns to load in the browser
 		files: [
 			'bower_components/jquery/dist/jquery.js',
-			'bower_components/jquery-ui/jquery-ui.js',
 			'bower_components/jquery-simulate/jquery.simulate.js',
 			'bower_components/javascript-detect-element-resize/jquery.resize.js',
 			'bower_components/angular/angular.js',
