584a55706e8070bfee88d7aa2ab520270c40ef23
Heather Roberts <heather.roberts@roames.com.au>
2015-07-17 09:23:56 +1000

    Reworking UMD structure to work with gulp, bower & browserify

--- src/angular-gridster.js
+++ src/angular-gridster.js
@@ -1,4 +1,20 @@
-(function(angular) {
+/*global define:true*/
+(function(root, factory) {
+
+	'use strict';
+
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['angular'], factory);
+	} else if (typeof exports === 'object') {
+		// CommonJS
+		require('angular');
+		module.exports = factory(angular);
+	} else {
+		// Browser, nothing "exported". Only registered as a module with angular.
+		factory(root.angular);
+	}
+}(this, function(angular) {
 
 	'use strict';
 
@@ -2316,4 +2332,4 @@
 
 	;
 
-})(angular);
+}));
