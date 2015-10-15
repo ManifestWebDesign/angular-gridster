4e9e12303b6891d8c89f20a42074049a745d92a7
jocluz <joc.luz@gmail.com>
2015-10-15 16:53:12 -0300

    Changed module registration

--- src/angular-gridster.js
+++ src/angular-gridster.js
@@ -1,20 +1,4 @@
-/*global define:true*/
-(function(root, factory) {
-
-	'use strict';
-
-	if (typeof define === 'function' && define.amd) {
-		// AMD
-		define(['angular'], factory);
-	} else if (typeof exports === 'object') {
-		// CommonJS
-		module.exports = factory(require('angular'));
-	} else {
-		// Browser, nothing "exported". Only registered as a module with angular.
-		factory(root.angular);
-	}
-}(this, function(angular) {
-
+(function() {
 	'use strict';
 
 	// This returned angular module 'gridster' is what is exported.
@@ -2210,4 +2194,4 @@ function($parse, GridsterDraggable, GridsterResizable, gridsterDebounce) {
 
 ;
 
-}));
+})();
