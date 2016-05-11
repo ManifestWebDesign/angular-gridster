0b7228ab8b88679134c7ae0acc7d76b7a1e21e43
Dan Blaisdell <dan@manifestwebdesign.com>
2016-05-10 22:23:27 -0700

    fix broken tests

--- karma.conf.js
+++ karma.conf.js
@@ -56,9 +56,7 @@ module.exports = function(config) {
 		// - Safari (only Mac)
 		// - PhantomJS
 		// - IE (only Windows)
-		browsers: ['PhantomJS'],
-
-
+		browsers: ['PhantomJS2'],
 		// Continuous Integration mode
 		// if true, it capture browsers, run tests and exit
 		singleRun: false
