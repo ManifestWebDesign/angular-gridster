3912fa984640a55bc24fa8350ab48ff3a7389670
Michael Zoumperakis <michael.zoumperakis@webtrekk.com>
2017-05-10 09:50:56 +0200

    Upgrades lodash version to 4.17.4

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
 			'bower_components/javascript-detect-element-resize/jquery.resize.js',
 			'bower_components/angular/angular.js',
 			'bower_components/angular-mocks/angular-mocks.js',
-			'bower_components/lodash/lodash.js',
+			'bower_components/lodash/dist/lodash.min.js',
 			'src/angular-gridster.module.js',
 			'src/**/*.js',
 			'test/spec/*.js'
